49867a9df179dbbd3aa3d26aecac69951c7b13c4
Pallaro <16.apallaro.2b@gmail.com>
2020-04-10 15:15:24 -0500

    game class

---
+++ src/main/java/game/Game.java
@@ -0,0 +1,19 @@
+package game;
+
+import programs.Program;
+
+public class Game {
+	static Program program;
+	static double IQ = 0;
+	Program[] elections;
+	//nivel
+	//tiempo
+	
+	public static void main(String[] args) {
+		program = new Program();
+		System.out.println(program.getName());
+		
+		
+	}
+
+}
